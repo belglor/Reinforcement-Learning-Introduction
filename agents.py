4af718ce20a732e77a21856f5e9659f760d789cc
faur <tokeee@gmail.com>
2018-05-28 16:26:44 +0200

    Make file stucture

---
+++ agents.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+
+# Tabular n-step Q
+# Tabular n-step SARSA
+
+# Linear n-step Q
+# Linear n-step SARSA
+
+# True SARSA
+# TB lambda
