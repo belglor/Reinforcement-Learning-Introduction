174ec731fd415e56d5b418057b131c66fa26159f
faur <tokeee@gmail.com>
2018-06-05 14:21:50 +0200

    Tabular code working

--- agents.py
+++ agents.py
@@ -2,10 +2,96 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 
 
-# Tabular n-step Q
-# Tabular n-step SARSA
+class TabularNStepQLearning:
+    def __init__(self, state_shape, num_actions, n=1):
+        self.num_actions = num_actions
+        self.tab_shape = np.hstack([state_shape, num_actions])
+        self.n = n
+
+        self.min_eps = 0.1
+        self.decay_len = 1 # 1e4
+        self.alpha = 0.1 / n
+        self.gamma = 1
+        self.t = 0
+        self.exp = []
+        self.Qtable = np.zeros(self.tab_shape)
+
+    @property
+    def eps(self):
+        _eps = 1 - self.t/self.decay_len
+        return np.maximum(self.min_eps, _eps)
+
+    def action(self, state):
+        self.t += 1
+        if np.random.uniform() > self.eps:
+            a = np.argmax(self.Qtable[tuple(state)])
+        else:
+            a = np.random.randint(self.num_actions)
+        return a
+
+    def compute_G(self):
+        """ Discounted reward"""
+        G = 0
+        for i in range(len(self.exp)):
+            G = G + (self.gamma ** i) * self.exp[i][2]
+        return G
+
+    def update(self, s, a, r, s_, d, **kwargs):
+        self.exp.append([s, a, r, s_])
+
+        # Qmax = np.max(self.Qtable[tuple(s_)])
+        # Q_as = self.Qtable[tuple(np.hstack([s, a]))]
+        #
+        # self.Qtable[tuple(np.hstack([s, a]))] = Q_as + self.alpha * (r + self.gamma * Qmax - Q_as)
+
+        if d:  # Done --> loop through experience
+            while len(self.exp) > 0:
+                fs, fa, fr, fs_ = self.exp[0]
+                G = self.compute_G()
+                self.exp.pop(0)
+
+                Q_fas = self.Qtable[tuple(np.hstack([fs, fa]))]
+                self.Qtable[tuple(np.hstack([fs, fa]))] = Q_fas + self.alpha * (G - Q_fas)
+
+        elif len(self.exp) < self.n:  # to early -- move along
+            pass
+        else:  # Normal n-step update w. bootstrapping
+            fs, fa, fr, fs_ = self.exp[0]
+            G = self.compute_G()
+            G += (self.gamma ** self.n) * np.max(self.Qtable[tuple(s_)])
+            self.exp.pop(0)
+
+            Q_fas = self.Qtable[tuple(np.hstack([fs, fa]))]
+            self.Qtable[tuple(np.hstack([fs, fa]))] = Q_fas + self.alpha * (G - Q_fas)
+
+
+class TabularNStepSARSA(TabularNStepQLearning):
+    def update(self, s, a, r, s_, a_, d, **kwargs):
+        self.exp.append([s, a, r, s_, a_])
+        
+        if d:  # Done --> loop through experience
+            while len(self.exp) > 0:
+                fs, fa, fr, fs_, fa_ = self.exp[0]
+                G = self.compute_G()
+                self.exp.pop(0)
+                
+                Q_fas = self.Qtable[tuple(np.hstack([fs, fa]))]
+                self.Qtable[tuple(np.hstack([fs, fa]))] = Q_fas + self.alpha * (G - Q_fas)
+            
+        elif len(self.exp) <= self.n:  # to early -- move along
+            pass
+        else:  # Normal n-step update w. bootstrapping
+            fs, fa, fr, fs_, fa_ = self.exp[0]
+            G = self.compute_G()
+            G += (self.gamma**self.n) * self.Qtable[tuple(np.hstack([s_, a_]))]
+            self.exp.pop(0)
+
+            Q_fas = self.Qtable[tuple(np.hstack([fs, fa]))]
+            self.Qtable[tuple(np.hstack([fs, fa]))] = Q_fas + self.alpha * (G - Q_fas)
+
 
 # Linear n-step Q
 # Linear n-step SARSA
