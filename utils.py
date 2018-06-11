9e592fdabc7a338f9687fb9d2ac3929fa7888747
s161174 <s161174@student.dtu.dk>
2018-06-11 18:10:54 +0200

    (possibly) final version of notebook; included costToGo and approx_run_loop to utils.py

--- utils.py
+++ utils.py
@@ -7,10 +7,10 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 class RunningPlot():
     def __enter__(self):
-        plt.clf()
+        plt.close()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        plt.draw()
+        #plt.draw()
         plt.pause(.1)
         return 0
 
@@ -34,3 +34,59 @@ def reward_plotter(rewards, title, col, smooth_factor=2, include_sd=False):
         plt.plot(means - sds, col, alpha=0.1)
     plt.legend()
     
+def costToGo(res, env, agent):
+    linspaces = []
+    for i in range(len(env.l_bound)): #do not consider number of tiles
+        linspaces.append(np.linspace(env.l_bound[i],env.h_bound[i],num=res))  
+                    
+    costToGo = np.zeros([res, res, env.nactions()])
+
+    for i in range(len(linspaces[0])):
+        for j in range(len(linspaces[1])):
+            s = env.encode([linspaces[0][i], linspaces[1][j]])
+            qsa = agent.linapprox(s)
+            costToGo[i,j,:] = qsa
+    costToGo = -np.amax(costToGo,axis=2)
+    return costToGo
+
+#TODO: unify this with utils.run_loop so it is accomodated in both
+
+def approx_run_loop(env, agent, title, max_e=None):
+    t = 0; i = 0; e = 0
+    s, r, d, _ = env.reset()
+    a_ = agent.action(s)
+    ep_lens = []; rewards = []
+    r_sum = 0
+    since_last_plot = 0
+
+    while True:
+        i += 1; t += 1; since_last_plot += 1
+        a = a_
+        s_, r, d, _ = env.step(a)
+        a_ = agent.action(s_)
+
+        agent.update(s=s, a=a, r=r, s_=s_, a_=a_, d=d)
+        r_sum += r
+        s = np.copy(s_)
+
+        if (e + 1) % 100 == 0:
+            env.render()            
+
+        if d:
+            if (e + 1) % 100 == 0:
+                with RunningPlot():
+                    plt.figure(1, figsize=(4, 4))
+                    plt.imshow(costToGo(128, env, agent))
+                    plt.title(title + ', episode: {}'.format(e) + ', step: {}'.format(i))
+                    plt.colorbar()
+        
+                
+            ep_lens.append(i)
+            rewards.append(r_sum)
+            r_sum = 0; e += 1; i = 0
+            s, r, d, _ = env.reset()
+
+        if max_e and e >= max_e:
+            break
+
+    return ep_lens, rewards
